389c973e0478971af3e8c1a46440eaccbd5ac944
chengxiaofei <971708992@qq.com>
2018-05-25 00:49:37 +0800

    抱错的第一步

---
+++ react1/config/webpack/webpack.base.conf.js
@@ -0,0 +1,10 @@
+let config = {
+    enter:{
+        'index':'./entryBuild/index.js'
+    },
+    resolve:{
+        extensions:[".js",".json",".jsx",".css",".pcss"]
+    }
+}
+
+module.exports = config
